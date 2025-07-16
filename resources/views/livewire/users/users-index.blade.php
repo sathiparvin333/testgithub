15303e927983f1852d07a9901f68bef609de5cbf
sadiakhatun193 <sadiakhatun193@gmail.com>
2025-07-16 12:01:45 +0600

    test

---
+++ resources/views/livewire/users/users-index.blade.php
@@ -0,0 +1,6 @@
+<div>
+    
+        <flux:heading size="xl" level="1">{{ __('Settings') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ __('Manage all your users ') }}</flux:subheading>
+    <flux:separator variant="subtle" />
+</div>
